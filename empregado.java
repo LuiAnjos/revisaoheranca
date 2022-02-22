b562a1e71d276945fa9e5053c5ed8073ac8e6456
LuiAnjos <luiluianjos@gmail.com>
2022-02-22 17:51:35 +0000

    inclusão da classe empregado

---
+++ empregado.java
@@ -0,0 +1,12 @@
+public class empregado extends pessoa{
+    
+    private double salario;
+
+    public double getSalario()[
+        return this.salario;
+    ]
+    public void setSalario(double salario){
+        this.salario=salario;
+    }
+}
+
